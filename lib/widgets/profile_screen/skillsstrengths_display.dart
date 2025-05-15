f4ae6591fd2419aa8e7c9f3bef6481bd5c2fdef4
Adrian <adrianrolfo1@gmail.com>
2025-05-15 15:28:36 +1000

    actual merging adrian and wilson
    cleaned profile_screen
    
    separated each display attribute widget into its own stateful widget class located in
    widgets/profile_screen/

--- lib/widgets/profile_screen/skillsstrengths_display.dart
+++ lib/widgets/profile_screen/skillsstrengths_display.dart
@@ -1,1 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_innatex_student_screens/core/models/skills_strengths_model.dart';
+import 'package:flutter_innatex_student_screens/features/profile_edit/edit_profile_attributes/edit_individual/edit_skills_strength.dart';
+import 'package:flutter_innatex_student_screens/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
+class MyExistingSkillsStrengthsWidget extends StatefulWidget {
+  const MyExistingSkillsStrengthsWidget({super.key});
+
+  @override
+  State<MyExistingSkillsStrengthsWidget> createState() =>
+      _MyExistingSkillsStrengthsWidgetState();
+}
+
+class _MyExistingSkillsStrengthsWidgetState
+    extends State<MyExistingSkillsStrengthsWidget> {
+  SkillsStrengthsModel? selectedSkillStrength;
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    ThemeData theme = Theme.of(context);
+    List<SkillsStrengthsModel> skills = userProvider.skillsStrengths;
+
+    if (skills.isEmpty) {
+      return Text(
+        'Add some skills & strengths!',
+        style: theme.textTheme.displayMedium,
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:
+            skills.map((skillModel) {
+              return GestureDetector(
+                onTap: () {
+                  if (selectedSkillStrength == skillModel) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => MyEditSkillStrengthScreen(
+                              skillStrength: skillModel,
+                            ),
+                      ),
+                    );
+                  }
+                  setState(() {
+                    selectedSkillStrength = skillModel;
+                  });
+                },
+                onLongPress: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => MyEditSkillStrengthScreen(
+                            skillStrength: skillModel,
+                          ),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border:
+                        skillModel == selectedSkillStrength
+                            ? Border.all(color: theme.primaryColor, width: 3)
+                            : null,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Divider(thickness: 1),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // No ellipsis — allow wrapping
+                          Flexible(
+                            child: Text(
+                              skillModel.skill,
+                              style: theme.textTheme.bodyMedium!.copyWith(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            skillModel.acquiredAt,
+                            style: theme.textTheme.bodyMedium!.copyWith(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        skillModel.description,
+                        style: theme.textTheme.displayMedium,
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+      );
+    }
+  }
+}
--- lib/widgets/profile_screen/volunteeringworks_display.dart
+++ lib/widgets/profile_screen/volunteeringworks_display.dart
@@ -1,1 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_innatex_student_screens/core/models/volunteering_work_model.dart';
+import 'package:flutter_innatex_student_screens/features/profile_edit/edit_profile_attributes/edit_individual/edit_volunteering_work.dart';
+import 'package:flutter_innatex_student_screens/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
+class MyExistingVolunteeringWorksWidget extends StatefulWidget {
+  const MyExistingVolunteeringWorksWidget({super.key});
+
+  @override
+  State<MyExistingVolunteeringWorksWidget> createState() =>
+      _MyExistingVolunteeringWorksWidgetState();
+}
+
+class _MyExistingVolunteeringWorksWidgetState
+    extends State<MyExistingVolunteeringWorksWidget> {
+  VolunteeringWorkModel? selectedVolunteeringWork;
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    ThemeData theme = Theme.of(context);
+    List<VolunteeringWorkModel> volunteeringWorks =
+        userProvider.volunteeringWork;
+
+    if (volunteeringWorks.isEmpty) {
+      return Text(
+        'Add some volunteering work!',
+        style: theme.textTheme.displayMedium,
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:
+            volunteeringWorks.map((work) {
+              final String startDate =
+                  '${work.dateStarted.toLocal().toString().split(' ')[0]}';
+              final String endDate =
+                  '${work.dateEnded?.toLocal().toString().split(' ')[0] ?? "Present"}';
+
+              return GestureDetector(
+                onTap: () {
+                  if (selectedVolunteeringWork == work) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => MyEditVolunteeringWorkScreen(
+                              volunteeringWork: work,
+                            ),
+                      ),
+                    );
+                  }
+                  setState(() {
+                    selectedVolunteeringWork = work;
+                  });
+                },
+                onLongPress: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => MyEditVolunteeringWorkScreen(
+                            volunteeringWork: work,
+                          ),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border:
+                        work == selectedVolunteeringWork
+                            ? Border.all(color: theme.primaryColor, width: 3)
+                            : null,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Divider(thickness: 1),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              work.institutionName,
+                              style: theme.textTheme.bodyMedium!.copyWith(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                startDate,
+                                style: theme.textTheme.bodyMedium!.copyWith(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                endDate,
+                                style: theme.textTheme.bodyMedium!.copyWith(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Text(work.role, style: theme.textTheme.displayMedium),
+                      const SizedBox(height: 4),
+                      Text(
+                        work.description,
+                        style: theme.textTheme.displayMedium,
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+      );
+    }
+  }
+}
