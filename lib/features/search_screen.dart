af8e48476d607bbc5133db6d6469f21796b60c75
Hong <o.xianghong@gmail.com>
2025-05-20 02:53:48 +1000

    Added Endorsement Filter Button to Search Screen

--- lib/features/search_screen.dart
+++ lib/features/search_screen.dart
@@ -12,6 +12,7 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   var searchName = "";
+  bool filterEndorsement = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,64 +22,99 @@ class _SearchScreenState extends State<SearchScreen> {
     return Theme(
       data: theme,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: theme.primaryColor,
-          title: SizedBox(
-            height: 40,
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  searchName = value;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search, color: Colors.white),
-              ),
-            ),
-          ),
-        ),
-
-        body: StreamBuilder<QuerySnapshot>(
-          stream:
-              FirebaseFirestore.instance
-                  .collection('users')
-                  .orderBy('name')
-                  .startAt([searchName])
-                  .endAt([
-                    '$searchName\uf8ff',
-                  ]) // Allow you to get all character after the searched name
-                  .limit(10)
-                  .snapshots(),
-
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('Something went wrong');
-            }
-
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loading");
-            }
-
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                var data = snapshot.data!.docs[index];
-                return ListTile(
-                  onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => MyProfileScreen(userId: data['id']),
-                    //   ),
-                    // );
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 8, 10, 0),
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      searchName = value;
+                    });
                   },
-                  title: Text(data['name']),
-                  subtitle: Text(data['email']),
-                );
-              },
-            );
-          },
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search, color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                child: SizedBox(
+                  width: 350,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        filterEndorsement = !filterEndorsement;
+                      });
+                    },
+                    child: Text('Endorsed?'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream:
+                      filterEndorsement
+                          ? FirebaseFirestore.instance
+                              .collection('users')
+                              .where('isEndorsed', isEqualTo: true)
+                              .orderBy('name')
+                              .startAt([searchName])
+                              .endAt(
+                                ['$searchName\uf8ff'],
+                              ) // Allow you to get all character after the searched name
+                              .limit(20)
+                              .snapshots()
+                          : FirebaseFirestore.instance
+                              .collection('users')
+                              .orderBy('name')
+                              .startAt([searchName])
+                              .endAt(
+                                ['$searchName\uf8ff'],
+                              ) // Allow you to get all character after the searched name
+                              .limit(20)
+                              .snapshots(),
+
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading");
+                    }
+
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        var data = snapshot.data!.docs[index];
+                        return ListTile(
+                          onTap: () {
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (context) => MyProfileScreen(userId: data['id']),
+                            //   ),
+                            // );
+                          },
+                          title: Text(data['name']),
+                          subtitle: Text(data['email']),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
