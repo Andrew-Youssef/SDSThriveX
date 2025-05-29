6f0130534d5a26df3e52a2f483e48aa0d2e94a7b
Andrew Youssef <andrew6546677@gmail.com>
2025-05-30 02:37:37 +1000

    Projects Integration Tests done
    Fixed 'add project' and 'edit project' Integration Tests so that they interact with the new UI properly.
    
    Added 'delete project' Integration Test

--- integration_test/projects_test.dart
+++ integration_test/projects_test.dart
@@ -47,12 +47,12 @@ void main() {
     await tester.enterText(find.byType(TextField).at(0), 'Test Project'); // project title
     await tester.pumpAndSettle();
 
-    await tester.tap(find.byIcon(Icons.calendar_today).first);
+    await tester.tap(find.byType(TextField).at(1));
     await tester.pumpAndSettle();
     await tester.tap(find.text('OK')); // letting it default to today's date
     await tester.pumpAndSettle();
 
-    await tester.tap(find.byIcon(Icons.calendar_today).last);
+    await tester.tap(find.byType(TextField).at(2));
     await tester.pumpAndSettle();
     await tester.tap(find.text('OK')); // letting it default to today's date
     await tester.pumpAndSettle();
@@ -61,50 +61,93 @@ void main() {
     await tester.pumpAndSettle();
 
     // Adding the project
-    await tester.tap(find.byIcon(Icons.add_box));
-    await tester.pumpAndSettle(const Duration(seconds: 5));
+    final addProjectButton = find.widgetWithText(ElevatedButton, 'Add new project!');
+    await tester.tap(addProjectButton);
+    await tester.pumpAndSettle(const Duration(seconds: 10));
 
     // Confirm project appears
     expect(find.text('Test Project'), findsWidgets);
-    expect(find.text('A project from integration test'), findsWidgets);
   });
 
-  // testWidgets('Edit an existing project', (WidgetTester tester) async {
-  //   app.main();
-  //   await tester.pumpAndSettle(const Duration(seconds: 10));
+  testWidgets('Edit an existing project', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
 
-  //   // Login
-  //   await tester.enterText(find.byType(TextFormField).first, 'AndrewTest1@gmail.com');
-  //   await tester.enterText(find.byType(TextFormField).last, 'Andrew1!');
-  //   await tester.tap(find.widgetWithText(ElevatedButton, 'Sign in'));
-  //   await tester.pumpAndSettle(const Duration(seconds: 5));
+    // Login
+    await tester.enterText(find.byType(TextFormField).first, 'AndrewTest1@gmail.com');
+    await tester.enterText(find.byType(TextFormField).last, 'Andrew1!');
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Sign in'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
 
-  //   // Go to Profile page
-  //   final profileTabFinder = find.byIcon(Icons.person_outlined);
-  //   await tester.pumpUntilFound(profileTabFinder, const Duration(seconds: 10));
-  //   await tester.tap(profileTabFinder);
-  //   await tester.pumpAndSettle(const Duration(seconds: 5));
+    // Go to Profile page
+    final profileTabFinder = find.byIcon(Icons.person_outlined);
+    await tester.pumpUntilFound(profileTabFinder, const Duration(seconds: 10));
+    await tester.tap(profileTabFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
 
-  //   // Long press the project card to open Edit Project page
-  //   final projectCardFinder = find.text('Test Project').first;
-  //   await tester.ensureVisible(projectCardFinder);
-  //   await tester.pumpAndSettle();
-  //   await tester.longPress(projectCardFinder);
-  //   await tester.pumpAndSettle(const Duration(seconds: 5));
+    // Long press the project card to open Edit Project page
+    final projectCardFinder = find.text('Test Project').first;
+    await tester.ensureVisible(projectCardFinder);
+    await tester.pumpAndSettle();
+    await tester.longPress(projectCardFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
 
-  //   // Modify the name and description
-  //   await tester.enterText(find.byType(TextField).at(0), 'Edited Project Title');
-  //   await tester.enterText(find.byType(TextField).at(3), 'Edited project description');
-  //   await tester.pumpAndSettle(const Duration(seconds: 2));
+    // Modify the name and description
+    await tester.enterText(find.byType(TextField).at(0), 'Edited Test Project Title');
+    await tester.enterText(find.byType(TextField).at(3), 'Edited test project description');
+    await tester.pumpAndSettle(const Duration(seconds: 2));
 
-  //   // Go back to Profile page
-  //   tester.state<NavigatorState>(find.byType(Navigator)).pop();
-  //   await tester.pumpAndSettle(const Duration(seconds: 5));
+    // Tap Confirm Details button
+    final endorsedButton = find.text("Confirm changes?");
+    await tester.tap(endorsedButton);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
 
-  //   // Confirm the updates appear in the Projects section
-  //   expect(find.text('Edited Project Title'), findsWidgets);
-  //   expect(find.text('Edited project description'), findsWidgets);
-  // });
+    // Go back to Profile page
+    tester.state<NavigatorState>(find.byType(Navigator)).pop();
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Confirm the updates appear in the Projects section
+    await tester.ensureVisible(find.text('Edited Test Project Title'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+    expect(find.text('Edited Test Project Title'), findsWidgets);
+  });
+
+  testWidgets('Delete an existing project', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
+
+    // Login
+    await tester.enterText(find.byType(TextFormField).first, 'AndrewTest1@gmail.com');
+    await tester.enterText(find.byType(TextFormField).last, 'Andrew1!');
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Sign in'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Go to Profile page
+    final profileTabFinder = find.byIcon(Icons.person_outlined);
+    await tester.pumpUntilFound(profileTabFinder, const Duration(seconds: 10));
+    await tester.tap(profileTabFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Long press the project card to open Edit Project page
+    final projectCardFinder = find.text('Edited Test Project Title').first;
+    await tester.ensureVisible(projectCardFinder);
+    await tester.pumpAndSettle();
+    await tester.longPress(projectCardFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Press bin button
+    final binButton = find.byIcon(Icons.delete);
+    await tester.tap(binButton);
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+
+    // Press confirm delete button
+    final confirmDeleteButton = find.widgetWithText(ElevatedButton, 'Delete');
+    await tester.tap(confirmDeleteButton);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Confirm that the project no longer exists in the Projects section
+    expect(find.text('Edited Test Project Title'), findsNothing);
+  });
 }
 
 extension WidgetTesterExtension on WidgetTester {
