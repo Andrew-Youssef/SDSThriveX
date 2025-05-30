eeb98a251d578d3a3f32d3f27c4bedf5f38b13e0
Andrew Youssef <andrew6546677@gmail.com>
2025-05-30 15:15:02 +1000

    Skills/Strengths Integration Tests done
    Fixed 'add skill' Integration Test so that it interacts with the new UI properly.
    
    Added 'edit skill' Integration Test.
    
    Added 'delete skill' Integration Test.

--- integration_test/skills_test.dart
+++ integration_test/skills_test.dart
@@ -56,12 +56,92 @@ void main() {
     await tester.pumpAndSettle();
 
     // Adding the skill
-    await tester.tap(find.byIcon(Icons.add_box));
-    await tester.pumpAndSettle(const Duration(seconds: 5));
+    final addSkillButton = find.widgetWithText(ElevatedButton, 'Add new skill/strength!');
+    await tester.tap(addSkillButton);
+    await tester.pumpAndSettle(const Duration(seconds: 10));
 
     // Confirm skill appears
     expect(find.text('Test Skill name'), findsWidgets);
-    expect(find.text('Test skill description'), findsWidgets);
+  });
+
+  testWidgets('Edit an existing Skill or Strength', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
+
+    // Login
+    await tester.enterText(find.byType(TextFormField).first, 'AndrewTest1@gmail.com');
+    await tester.enterText(find.byType(TextFormField).last, 'Andrew1!');
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Sign in'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Go to Profile page
+    final profileTabFinder = find.byIcon(Icons.person_outlined);
+    await tester.pumpUntilFound(profileTabFinder, const Duration(seconds: 10));
+    await tester.tap(profileTabFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Long press the skill card to open Edit Skill or Strength page
+    final skillCardFinder = find.text('Test Skill name').first;
+    await tester.ensureVisible(skillCardFinder);
+    await tester.pumpAndSettle();
+    await tester.longPress(skillCardFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Modify the name and description
+    await tester.enterText(find.byType(TextField).at(0), 'Edited Test Skill name');
+    await tester.enterText(find.byType(TextField).at(2), 'Edited test skill description');
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+
+    // Tap Confirm Details button
+    final confirmButton = find.text("Confirm changes?");
+    await tester.tap(confirmButton);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Go back to Profile page
+    tester.state<NavigatorState>(find.byType(Navigator)).pop();
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Confirm the updates appear in the Skills & Strengths section
+    await tester.ensureVisible(find.text('Edited Test Skill name'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+    expect(find.text('Edited Test Skill name'), findsWidgets);
+  });
+
+  testWidgets('Delete an Skill or Strength', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
+
+    // Login
+    await tester.enterText(find.byType(TextFormField).first, 'AndrewTest1@gmail.com');
+    await tester.enterText(find.byType(TextFormField).last, 'Andrew1!');
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Sign in'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Go to Profile page
+    final profileTabFinder = find.byIcon(Icons.person_outlined);
+    await tester.pumpUntilFound(profileTabFinder, const Duration(seconds: 10));
+    await tester.tap(profileTabFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Long press the skill card to open Edit Skill or Strength page
+    final skillCardFinder = find.text('Edited Test Skill name').first;
+    await tester.ensureVisible(skillCardFinder);
+    await tester.pumpAndSettle();
+    await tester.longPress(skillCardFinder);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Press bin button
+    final binButton = find.byIcon(Icons.delete);
+    await tester.tap(binButton);
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+
+    // Press confirm delete button
+    final confirmDeleteButton = find.widgetWithText(ElevatedButton, 'Delete');
+    await tester.tap(confirmDeleteButton);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    // Confirm that the skill no longer exists in the Skills & Strengths section
+    expect(find.text('Edited Test Skill name'), findsNothing);
   });
 }
 
